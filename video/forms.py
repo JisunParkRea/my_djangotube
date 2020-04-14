3529106f281e5f87fda7b21e6f7be24513a90cf0
Jisun Park <fpdldk912@gmail.com>
2020-04-14 21:19:28 +0900

    add forms and modify views

---
+++ video/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Video
+
+class VideoForm(forms.ModelForm):
+
+    class Meta:
+        model = Video
+        fields = ['title', 'video_key']
--- video/views.py
+++ video/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Video
+from .forms import VideoForm
 
 def video_list(request):
     video_list = Video.objects.all()
@@ -7,9 +8,8 @@ def video_list(request):
 
 def video_new(request):
     if request.method == 'POST': # 새로운 비디오 데이터를 업로드할 때
-        title = request.POST['title']
-        video_key = request.POST['video_key']
-        Video.objects.create(title=title, video_key=video_key)
+        form = VideoForm(request.POST)
+        form.save()
         return redirect('video_list')
     elif request.method == 'GET': # 새로운 비디오를 추가할 템플릿을 가져와야할 때
         return render(request, 'video/video_new.html')
