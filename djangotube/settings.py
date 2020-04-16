882a0b456fda5a041526de10741e9f40657a7bb0
Jisun Park <fpdldk912@gmail.com>
2020-04-16 15:56:26 +0900

    change db, sqlite3 to postgresql

--- djangotube/settings.py
+++ djangotube/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'djangotube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangotube',
+        'USER': 'postgres',
+        'PASSWORD': 'qhanf1gh',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
