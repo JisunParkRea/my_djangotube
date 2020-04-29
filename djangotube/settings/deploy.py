245965f147c90a226a89777db17ec1a7cfbc5ff8
Jisun Park <fpdldk912@gmail.com>
2020-04-29 18:39:47 +0900

    change location of django heroku settings code

--- djangotube/settings/deploy.py
+++ djangotube/settings/deploy.py
@@ -3,8 +3,6 @@ import dj_database_url
 import django_heroku
 
 
-django_heroku.settings(locals())
-
 DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -30,6 +28,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+django_heroku.settings(locals())
+
 # WSGI application
 WSGI_APPLICATION = 'djangotube.wsgi.deploy.application'
 
@@ -38,6 +38,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 STATIC_DIR = os.path.join(BASE_DIR, 'djangotube', 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
